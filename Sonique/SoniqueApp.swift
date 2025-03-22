53bbb0b520e843b897941367a4f6f28bf159e3fa
manonmission88 <manishnewray88@gmail.com>
2025-03-22 16:31:28 -0400

    Initial Commit

---
+++ Sonique/SoniqueApp.swift
@@ -0,0 +1,17 @@
+//
+//  SoniqueApp.swift
+//  Sonique
+//
+//  Created by Manish Niure on 3/22/25.
+//
+
+import SwiftUI
+
+@main
+struct SoniqueApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ SoniqueTests/SoniqueTests.swift
@@ -0,0 +1,17 @@
+//
+//  SoniqueTests.swift
+//  SoniqueTests
+//
+//  Created by Manish Niure on 3/22/25.
+//
+
+import Testing
+@testable import Sonique
+
+struct SoniqueTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
